025c95e2001809e7ab2872f1e498949c851e4e3c
DeepankarAcharyya <deepankaracharyya@gmail.com>
2019-08-22 16:40:40 +0530

    done 1a partly

--- Assignment-1a.c
+++ Assignment-1a.c
@@ -6,8 +6,6 @@ no. From parent display the PID and PPID of all the processes and display the co
 processes created also the number of grandchild processes created. Demonstrate the use of exit(0) and
 exit(1). 
 */
-
-//need to set up a shared memory
 #include<stdio.h>
 #include<stdlib.h>
 #include<sys/types.h>
@@ -15,6 +13,18 @@ exit(1).
 #include<sys/wait.h>
 
 int main(){
+    //using pipes to share the info between the processes
+    int shared_pid_data[2],shared_count_child[2],shared_count_grand[2];
+    //creating the pipes
+    int return_status1=pipe(shared_pid_data);
+    int return_status2=pipe(shared_count_child);
+    int return_status3=pipe(shared_count_grand);
+
+    if(return_status1<0 || return_status2<0 || return_status3<0 ){
+        printf("\nFailed to create pipe(s)!\n");
+        exit(1);
+    }
+
     printf("i am the parent with pid: %d\n",(int) getpid());
     pid_t pid=fork();
     
@@ -24,28 +34,59 @@ int main(){
     }
     else if(pid ==0){
         printf("\nI am the child \n Pid : %d\n PPID : %d\n",(int)getpid(),(int)getppid());
+        
+        // int count1;
+        // printf("\n12a\n");
+        // read(shared_count_child[0],&count1,sizeof(count1));
+        // printf("\n12a\n");
+        // count1++;
+        // printf("\n12a\n");
+        // write(shared_count_child[1],&count1,sizeof(count1));
+        // printf("\n12a\n");
+
         pid_t pid_grand=fork();
         if (pid_grand<0){
             perror("Fork Failed\n");
             exit(1);
         }
         else if(pid_grand==0){
+
+            // int count2;
+            // printf("\n13a\n");
+            // read(shared_count_grand[0],&count2,sizeof(count2));
+            // printf("\n13a\n");
+            // count2++;
+            // printf("\n13a\n");
+            // write(shared_count_grand[1],&count2,sizeof(count2));
+            // printf("\n13a\n");
+            int add=(int)getpid();
+            write(shared_pid_data[1],&add,sizeof(int));
+            printf("\n13a\n");
             printf("I am the Grand-child with Pid :%d\n",(int)getpid());
             printf("Pid:%d::My Roll-No is : CSB17017\n",(int)getpid());
             printf("\nExiting process with Pid:%d\n",(int)getpid());
             exit(0);
         }
+
         wait(NULL);
         printf("\nExiting process with Pid:%d\n",(int)getpid());
         int temp=(int)pid_grand;
         exit(temp);
+        
         }
-     
-    // printf("\nFrom the parent:\n");
-    // printf("\n For parent PID:%d\n", (int) getpid());
-    // printf("\n For child PID:%d and PPID:%d\n",(int)pid,(int) getpid());
-    // printf("\n For grandchild PID:%d and PPID:%d\n",grand,pid);
-    // //printf("\n count child:%d  grandchild:%d \n",a.count_child,a.count_grand);
-    // printf("\nExiting process with Pid:%d\n",(int)getpid());
+
+     wait(NULL);
+
+     int count_child,count_grand,pid_g;
+     read(shared_count_grand[0],&count_grand,sizeof(count_grand));
+     read(shared_count_child[0],&count_child,sizeof(count_child));
+     read(shared_pid_data[0],&pid_g,sizeof(pid_g));
+                   
+     printf("\n For parent PID:%d\n", (int) getpid());
+     printf("\n For child PID:%d and PPID:%d\n",(int)pid,(int) getpid());
+     printf("\n For grandchild PID:%d and PPID:%d\n",pid_g,(int)pid);
+     printf("\n count child:%d  grandchild:%d \n",count_child,count_grand);
+
+     printf("\nExiting process with Pid:%d\n",(int)getpid());
      return 0;
 }
