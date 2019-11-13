646d9a5cef132989de41d643c162febbc638213a
DeepankarAcharyya <deepankaracharyya@gmail.com>
2019-11-13 22:37:06 +0530

    1

---
+++ Assignment-71.c
@@ -0,0 +1,14 @@
+//Assignment 7
+/*
+Open a file -> read the contents
+*/
+
+#include <stdio.h>
+
+int main(){
+    //create a semaphore
+    //lock the file
+    //read the file n times--same as file 2
+    //the calls
+
+}
